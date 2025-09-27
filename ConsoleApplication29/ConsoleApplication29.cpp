41ddcd31970540e0f9f36853425bca1a721a0f18
Rovell72 <weatheryt28@gmail.com>
2025-09-27 17:59:17 +0300

    Update ConsoleApplication29.cpp

--- ConsoleApplication29/ConsoleApplication29.cpp
+++ ConsoleApplication29/ConsoleApplication29.cpp
@@ -1,82 +1,184 @@
-﻿#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+using namespace std;
 
 
 
-typedef struct {
-    char surname[50];
-    char name[50];
+struct Employee {
+    string surname;
+    string name;
     int age;
-} Employee;
+};
 
 Employee* employees = NULL;
-int count = 0;
+int countEmployees = 0;
+string filename;
 
 void addEmployee() {
-    employees = (Employee*)realloc(employees, (count + 1) * sizeof(Employee));
-    if (employees == NULL) {
-        printf("Memory allocation error!\n");
-        exit(1);
-    }
-    printf("Surname: ");
-    scanf("%49s", employees[count].surname);
-    printf("Name: ");
-    scanf("%49s", employees[count].name);
-    printf("Age: ");
-    scanf("%d", &employees[count].age);
-    count++;
+    employees = (Employee*)realloc(employees, (countEmployees + 1) * sizeof(Employee));
+    cout << "Surname: ";
+    cin >> employees[countEmployees].surname;
+    cout << "Name: ";
+    cin >> employees[countEmployees].name;
+    cout << "Age: ";
+    cin >> employees[countEmployees].age;
+    countEmployees++;
 }
 
 void showAll() {
-    if (count == 0) {
-        printf("List is empty\n");
+    if (countEmployees == 0) {
+        cout << "List is empty\n";
         return;
     }
-    for (int i = 0; i < count; i++) {
-        printf("%d) %s %s, %d years\n", i + 1,
-            employees[i].surname,
-            employees[i].name,
-            employees[i].age);
+    for (int i = 0; i < countEmployees; i++) {
+        cout << i + 1 << ") " << employees[i].surname << " "
+            << employees[i].name << ", " << employees[i].age << " years\n";
     }
 }
-void deleteEmployee() {
+
+void editEmployee() {
     int index;
-    printf("Enter index to delete (1-%d): ", count);
-    scanf("%d", &index);
+    cout << "Enter index to edit (1-" << countEmployees << "): ";
+    cin >> index;
     index--;
-    if (index < 0 || index >= count) {
-        printf("Invalid index!\n");
+    if (index < 0 || index >= countEmployees) {
+        cout << "Invalid index\n";
         return;
     }
-    for (int i = index; i < count - 1; i++) {
+    cout << "New surname: ";
+    cin >> employees[index].surname;
+    cout << "New name: ";
+    cin >> employees[index].name;
+    cout << "New age: ";
+    cin >> employees[index].age;
+}
+
+void deleteEmployee() {
+    int index;
+    cout << "Enter index to delete (1-" << countEmployees << "): ";
+    cin >> index;
+    index--;
+    if (index < 0 || index >= countEmployees) {
+        cout << "Invalid index\n";
+        return;
+    }
+    for (int i = index; i < countEmployees - 1; i++) {
         employees[i] = employees[i + 1];
     }
-    count--;
-    employees = (Employee*)realloc(employees, count * sizeof(Employee));
+    countEmployees--;
+    employees = (Employee*)realloc(employees, countEmployees * sizeof(Employee));
+}
+
+void searchBySurname() {
+    string s;
+    cout << "Enter surname to search: ";
+    cin >> s;
+    ofstream fout("search_result.txt");
+    for (int i = 0; i < countEmployees; i++) {
+        if (employees[i].surname == s) {
+            cout << employees[i].surname << " " << employees[i].name
+                << ", " << employees[i].age << " years\n";
+            fout << employees[i].surname << " " << employees[i].name
+                << ", " << employees[i].age << " years\n";
+        }
+    }
+    fout.close();
+}
+
+void filterByAge() {
+    int a;
+    cout << "Enter age: ";
+    cin >> a;
+    ofstream fout("filter_result.txt");
+    for (int i = 0; i < countEmployees; i++) {
+        if (employees[i].age == a) {
+            cout << employees[i].surname << " " << employees[i].name
+                << ", " << employees[i].age << " years\n";
+            fout << employees[i].surname << " " << employees[i].name
+                << ", " << employees[i].age << " years\n";
+        }
+    }
+    fout.close();
+}
+
+void filterByInitial() {
+    char c;
+    cout << "Enter initial letter: ";
+    cin >> c;
+    ofstream fout("filter_result.txt");
+    for (int i = 0; i < countEmployees; i++) {
+        if (!employees[i].surname.empty() && employees[i].surname[0] == c) {
+            cout << employees[i].surname << " " << employees[i].name
+                << ", " << employees[i].age << " years\n";
+            fout << employees[i].surname << " " << employees[i].name
+                << ", " << employees[i].age << " years\n";
+        }
+    }
+    fout.close();
+}
+
+void saveToFile() {
+    ofstream fout(filename);
+    for (int i = 0; i < countEmployees; i++) {
+        fout << employees[i].surname << " "
+            << employees[i].name << " "
+            << employees[i].age << "\n";
+    }
+    fout.close();
+}
+
+void loadFromFile() {
+    ifstream fin(filename);
+    if (!fin) return;
+    Employee e;
+    while (fin >> e.surname >> e.name >> e.age) {
+        employees = (Employee*)realloc(employees, (countEmployees + 1) * sizeof(Employee));
+        employees[countEmployees] = e;
+        countEmployees++;
+    }
+    fin.close();
 }
 
 int main() {
+    cout << "Enter file name: ";
+    cin >> filename;
+    loadFromFile();
     int choice;
     do {
-        printf("Menu\n");
-        printf("1. Add employee\n");
-        printf("2. Show all\n");
-        printf("3. Delete employee\n");
-        printf("0. Exit\n");
-        printf("Your choice: ");
-        scanf("%d", &choice);
-
+        cout << "Menu\n";
+        cout << "1 Add employee\n";
+        cout << "2 Show all\n";
+        cout << "3. Edit employee\n";
+        cout << "4 Delete employee\n";
+        cout << "5 Search by surname\n";
+        cout << "6 Filter by age\n";
+        cout << "7. Filter by initial letter\n";
+        cout << "8 Save to file\n";
+        cout << "0 Exit\n";
+        cout << "Your choice: ";
+        cin >> choice;
         switch (choice) {
         case 1: addEmployee(); break;
         case 2: showAll(); break;
-        case 3: deleteEmployee(); break;
+        case 3: editEmployee(); break;
+        case 4: deleteEmployee(); break;
+        case 5: searchBySurname(); break;
+        case 6: filterByAge(); break;
+        case 7: filterByInitial(); break;
+        case 8: saveToFile(); break;
         }
     } while (choice != 0);
-
+    saveToFile();
     free(employees);
     return 0;
 }
+
+
+
+
+
 
 
 
